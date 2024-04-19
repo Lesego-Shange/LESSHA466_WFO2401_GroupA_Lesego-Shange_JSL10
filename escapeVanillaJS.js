d27e35930813aee64880d5d5118b27e0708ad742
Lesego Shange <makgeledisal@gmail.com>
2024-04-19 16:29:18 +0200

    Task 7

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -49,7 +49,7 @@ function findMostRecentBook(books) {
 
 function findIntersection(setA, setB) {
   // 🪲 Bug: Incorrect logic
-  const intersection = new Set([...setA]);
+  const intersection = new Set([...setA].filter(x => setB.has(x)));
   return intersection;
 }
 
